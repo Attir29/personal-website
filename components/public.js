1c7610161ecd03d6c83c5855037935e6a1ba5384
Your Name <attirmidziislamipratama@gmail.com>
2026-01-14 20:26:05 +0700

    add portfolio on public.js

--- components/public.js
+++ components/public.js
@@ -1,26 +1,52 @@
-
 const pubSpeak = [
-    {
-        name: "input html elements",
-        link: "https://www.instagram.com/reel/DJoTky0vVC_/?utm_source=ig_web_copy_link&igsh=MzRlODBiNWFlZA==",
-        image: "/images/editteks.png",
-        description: "A simple and effective explanation on how to use the text input element in HTML—perfect for beginners learning basic web development.",
-        cta: "go to video"
-    },
-    {
-        name: "training with babinsa",
-        link: "https://www.instagram.com/reel/DKG5vXRvNnx/?utm_source=ig_web_copy_link&igsh=MzRlODBiNWFlZA==",
-        image: "/images/babinsa.png",
-        description: "In this session, I guided students through physical training and marching drills with the Indonesian military (BABINSA), focusing on discipline, teamwork, and responsibility.",
-        cta: "go to video"
-    },
-    {
-        name: "what's kodein berbagi?",
-        link: "https://www.instagram.com/reel/DKG7V6yvVhN/?utm_source=ig_web_copy_link&igsh=MzRlODBiNWFlZA==",
-        image: "/images/kodeinberbagi.png",
-        description: "Representing KODEIN, we helped share knowledge in programming and multimedia to nearby junior high schools, aiming to inspire and empower the next generation with tech skills.",
-        cta: "go to video"
-    }
-]
+  {
+    name: "input html elements",
+    link: "https://www.instagram.com/reel/DJoTky0vVC_/?utm_source=ig_web_copy_link&igsh=MzRlODBiNWFlZA==",
+    image: "/images/editteks.png",
+    description:
+      "A simple and effective explanation on how to use the text input element in HTML—perfect for beginners learning basic web development.",
+    cta: "go to video",
+  },
+  {
+    name: "training with babinsa",
+    link: "https://www.instagram.com/reel/DKG5vXRvNnx/?utm_source=ig_web_copy_link&igsh=MzRlODBiNWFlZA==",
+    image: "/images/babinsa.png",
+    description:
+      "In this session, I guided students through physical training and marching drills with the Indonesian military (BABINSA), focusing on discipline, teamwork, and responsibility.",
+    cta: "go to video",
+  },
+  {
+    name: "what's kodein berbagi?",
+    link: "https://www.instagram.com/reel/DKG7V6yvVhN/?utm_source=ig_web_copy_link&igsh=MzRlODBiNWFlZA==",
+    image: "/images/kodeinberbagi.png",
+    description:
+      "Representing KODEIN, we helped share knowledge in programming and multimedia to nearby junior high schools, aiming to inspire and empower the next generation with tech skills.",
+    cta: "go to video",
+  },
+  {
+    name: "Styling font with CSS",
+    link: "https://www.instagram.com/reel/DMpdOk5PG-G/?utm_source=ig_web_copy_link&igsh=MzRlODBiNWFlZA==",
+    image: "./css.jpeg",
+    description:
+      "A video that shows how to style fonts using CSS in a simple and practical way. Perfect for beginners who want to make their text look cleaner and more attractive on the web.",
+    cta: "go to video",
+  },
+  {
+    name: "Commitment from school",
+    link: "https://www.instagram.com/reel/DNK5y9KPCn1/?utm_source=ig_web_copy_link&igsh=MzRlODBiNWFlZA==",
+    image: "./ponpes.jpeg",
+    description:
+      "As time goes by, Islamic boarding schools are starting to lose their identity, instead of studying religion, they are deviating from the right path, that is where our school is committed.",
+    cta: "go to video",
+  },
+  {
+    name: "Certificate for future",
+    link: "https://www.instagram.com/reel/DP0whpQj4hq/?utm_source=ig_web_copy_link&igsh=MzRlODBiNWFlZA==",
+    image: "./sertifikat.jpeg",
+    description:
+      "Nowadays, just a high school diploma is not enough to apply for a job, because HRD can give additional value if we have a certificate.",
+    cta: "go to video",
+  },
+];
 
-export default pubSpeak
+export default pubSpeak;
